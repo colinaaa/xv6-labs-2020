aac5a3c90133dbf36c2cb7846c90696d19d97e26
Colin <colinwang.0616@gmail.com>
2020-09-12 20:19:40 +0800

    feat(pgtbl): implement vmprint
    not so hard

--- kernel/vm.c
+++ kernel/vm.c
@@ -131,7 +131,7 @@ kvmpa(uint64 va)
   uint64 off = va % PGSIZE;
   pte_t *pte;
   uint64 pa;
-  
+
   pte = walk(kernel_pagetable, va, 0);
   if(pte == 0)
     panic("kvmpa");
@@ -341,7 +341,7 @@ void
 uvmclear(pagetable_t pagetable, uint64 va)
 {
   pte_t *pte;
-  
+
   pte = walk(pagetable, va, 0);
   if(pte == 0)
     panic("uvmclear");
@@ -438,5 +438,53 @@ copyinstr(pagetable_t pagetable, char *dst, uint64 srcva, uint64 max)
     return 0;
   } else {
     return -1;
+  }
+}
+
+static char __vmprintprefix[][10] = {
+  "",
+  " ..",
+  " .. ..",
+  " .. .. .."
+};
+
+void
+vmprintpte(pagetable_t pagetable, int level)
+{
+  int i;
+
+  if (level > 3) {
+    return;
+  }
+
+  for (i = 0; i < 512; i++) {
+    pte_t pte = pagetable[i];
+
+    if (pte & PTE_V) {
+      uint64 child = PTE2PA(pte);
+
+      printf("%s%d: pte %p pa %p\n",
+          __vmprintprefix[level],
+          i,
+          pte & 0xffffffff,
+          child & 0xffffffff
+      );
+      vmprintpte((pagetable_t) child, level + 1);
+    }
+  }
+}
+
+void
+vmprint(pagetable_t pagetable)
+{
+  printf("page table %p\n", (uint64)pagetable & 0xffffffff);
+
+  for (int i = 0; i < 512; i++) {
+    pte_t pte = pagetable[i];
+
+    if (pte & PTE_V) {
+      printf("%s%d: pte %p pa %p\n", __vmprintprefix[1], i, pte & 0xffffffff, PTE2PA(pte) & 0xffffffff);
+      vmprintpte((pagetable_t) PTE2PA(pte), 1);
+    }
   }
 }
